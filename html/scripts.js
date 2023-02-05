fb29a999652ca17047396ec6341992cb81fc69f5
Zan1456 <62830223+Zan1456@users.noreply.github.com>
2023-02-05 13:28:31 +0100

    .

--- html/scripts.js
+++ html/scripts.js
@@ -1,120 +1,117 @@
-class Fahrschule {
-  aktuelleFrage = 1;
-  fehlerpunkte = 0;
-  maxFehlerpunkte = 9;
+class DrivingSchool {
+  currentQuestion = 1;
+  faultpoints = 0;
+  maxErrorPoints = 9;
   richtigeAntworten = 0;
   falscheAntworten = 0;
   bestanden = false;
 
-  fragen = {
+  questions = {
     1: {
-      frage: "If you're going 80 km/h, and you're approaching a residential area you must:",
-      antworten: [
+      question: "If you're going 80 km/h, and you're approaching a residential area you must:",
+      answer: [
         ["You accelerate", false],
         ["You keep your speed, if you do not pass other vehicles", false],
         ["You slow down", true],
         ["You keep your speed", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     2: {
-      frage: "If you're turning right at a traffic light, but see a pedestrian crossing what do you do:",
-      antworten: [
+      question: "If you're turning right at a traffic light, but see a pedestrian crossing what do you do:",
+      answer: [
         ["You pass the pedestrian", false],
         ["You check that there is no other vehicles around", false],
         ["You wait until the pedestrian has crossed", true],
         ["You shoot the pedestrian and continue to drive", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     3: {
-      frage: "Without any prior indication, the speed in a residential area is: __ km/h",
-      antworten: [
+      question: "Without any prior indication, the speed in a residential area is: __ km/h",
+      answer: [
         ["30 km/h", false],
         ["50 km/h", true],
         ["40 km/h", false],
         ["60 km/h", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     4: {
-      frage: "Before every lane change you must:",
-      antworten: [
+      question: "Before every lane change you must:",
+      answer: [
         ["Check your mirrors", true],
         ["Check your blind spots", true],
         ["Signal your intentions", true]
       ],
-      fehlerpunkte: 3
+      faultpoints: 3
     },
     5: {
-      frage: "What blood alcohol level is classified as driving while intoxicated?",
-      antworten: [
+      question: "What blood alcohol level is classified as driving while intoxicated?",
+      answer: [
         ["0.05%", false],
         ["0.18%", false],
         ["0.06%", true],
         ["0.08%", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     6: {
-      frage: "When can you continue to drive at a traffic light?",
-      antworten: [
+      question: "When can you continue to drive at a traffic light?",
+      answer: [
         ["When it is green", false],
         ["When there is nobody in the intersection", false],
         ["You are in a school zone", false],
         ["When it is green and / or red and you're turning right", true]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     7: {
-      frage: "A pedestrian has a do not cross signal, what do you do?",
-      antworten: [
+      question: "A pedestrian has a do not cross signal, what do you do?",
+      answer: [
         ["You let them pass", false],
         ["You observe before continuing", false],
         ["You wave to tell them to cross", false],
         ["You continue because your traffic light is green", true]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     8: {
-      frage: "What is allowed when passing another vehicle",
-      antworten: [
+      question: "What is allowed when passing another vehicle",
+      answer: [
         ["You follow it closely to pass it faster", false],
         ["You pass it without leaving the roadway", false],
         ["You drive on the opposite side of the road to pass", true],
         ["You exceed the speed limit to pass them", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     9: {
-      frage: "You are driving on a highway which indicates a maximum speed of 120 km/h. But most trafficers drive at 125 km/h, so you should not drive faster than:",
-      antworten: [
+      question: "You are driving on a highway which indicates a maximum speed of 120 km/h. But most trafficers drive at 125 km/h, so you should not drive faster than:",
+      answer: [
         ["120 km/h", true],
         ["125 km/h", false],
         ["130 km/h", false],
         ["110 km/h", false]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     },
     10: {
-      frage: "When you are overtaken by another vehicle it is important NOT to:",
-      antworten: [
+      question: "When you are overtaken by another vehicle it is important NOT to:",
+      answer: [
         ["Slow Down", false],
         ["Check your mirrors", false],
         ["Watch other drivers", false],
         ["Increase your speed", true]
       ],
-      fehlerpunkte: 4
+      faultpoints: 4
     }
   }
-
-
-
 }
 
-let fahrschule = new Fahrschule();
+let drivingSchool = new DrivingSchool();
 
-$(".start-fahrschule").click(function(e) {
+$(".start-drivingSchool").click(function(e) {
   startFahrschule();
 });
 
@@ -124,7 +121,7 @@ $(".question-submit").click(function(e) {
 
 
 $(".end-button").click(function(){
-  if(fahrschule.bestanden) {
+  if(drivingSchool.bestanden) {
     $.post('http://esx_dmvschool/close', JSON.stringify({}));
   } else {
     $.post('http://esx_dmvschool/kick', JSON.stringify({}));
@@ -134,12 +131,12 @@ $(".end-button").click(function(){
 window.addEventListener('message', function(event){
   var item = event.data;
   if(item.openQuestion == true) {
-    fahrschule = new Fahrschule();
+    drivingSchool = new DrivingSchool();
     newFahrschule();
-    $(".fahrschule").fadeIn();
+    $(".drivingSchool").fadeIn();
   }
   if(item.openQuestion == false) {
-    $(".fahrschule").hide(0);
+    $(".drivingSchool").hide(0);
   }
 });
 
@@ -153,14 +150,14 @@ function newFahrschule() {
 function startFahrschule() {
   $(".introduction").hide(0);
   $(".question").fadeIn(1000);
-  setQuestion(fahrschule.aktuelleFrage)
+  setQuestion(drivingSchool.currentQuestion)
   $(".step-progress__progress").animate({ width: "5%" }, 500 );
 }
 
 function endFahrschule() {
   $(".question").hide(0);
 
-  if(fahrschule.fehlerpunkte >= fahrschule.maxFehlerpunkte) {
+  if(drivingSchool.faultpoints >= drivingSchool.maxErrorPoints) {
     $(".evaluation-title").html("Oops, that was probably nothing!")
     $(".evaluation-description").html("Unfortunately, you did not pass the theoretical driving test.")
     $(".evaluation-text").html("You are welcome to repeat the theoretical driving test again.")
@@ -172,12 +169,12 @@ function endFahrschule() {
     $(".evaluation-text").html("You can now take the practical driving test.")
     $(".evaluation-text2").html("We wish you lots of fun driving.")
     $(".evaluation-text3").html("Thank you for the participation!")
-    fahrschule.bestanden = true;
+    drivingSchool.bestanden = true;
   }
 
-  $(".auswertung--falsch span").html(fahrschule.falscheAntworten)
-  $(".auswertung--richtig span").html(fahrschule.richtigeAntworten)
-  $(".auswertung--fehlerpunkte span").html(fahrschule.fehlerpunkte)
+  $(".evaluation--incorrect span").html(drivingSchool.falscheAntworten)
+  $(".evaluation--correct span").html(drivingSchool.richtigeAntworten)
+  $(".evaluation--faultpoints span").html(drivingSchool.faultpoints)
 
   $(".evaluation").fadeIn(1000);
 }
@@ -186,12 +183,12 @@ function endFahrschule() {
 function loadNextQuestion() {
   const checkQuestion = checkCurrentQuestion();
   if(checkQuestion) {
-    fahrschule.aktuelleFrage++;
-    if(fahrschule.aktuelleFrage > Object.keys(fahrschule.fragen).length) {
+    drivingSchool.currentQuestion++;
+    if(drivingSchool.currentQuestion > Object.keys(drivingSchool.questions).length) {
       endFahrschule();
     } else {
       resetQuestion()
-      setQuestion(fahrschule.aktuelleFrage)
+      setQuestion(drivingSchool.currentQuestion)
     }
   }
   updateStepProgress()
@@ -208,14 +205,14 @@ function checkCurrentQuestion() {
     $(".error-message").fadeOut(0)
 
     $("input[type=checkbox").each(function(e) {
-      if(fahrschule.fragen[fahrschule.aktuelleFrage].antworten[this.name][1] != this.checked) fehler = true;
+      if(drivingSchool.questions[drivingSchool.currentQuestion].answer[this.name][1] != this.checked) fehler = true;
     });
 
     if(fehler) {
-      fahrschule.fehlerpunkte += fahrschule.fragen[fahrschule.aktuelleFrage].fehlerpunkte
-      fahrschule.falscheAntworten++;
+      drivingSchool.faultpoints += drivingSchool.questions[drivingSchool.currentQuestion].faultpoints
+      drivingSchool.falscheAntworten++;
     } else {
-      fahrschule.richtigeAntworten++;
+      drivingSchool.richtigeAntworten++;
     }
 
 
@@ -227,22 +224,22 @@ function checkCurrentQuestion() {
 }
 
 function setQuestion(questionId) {
-  $(".section__header h1").html("["+questionId+"] "+fahrschule.fragen[questionId].frage+"")
-  $(".section__header .fehlerpunkte").html(""+fahrschule.fragen[questionId].fehlerpunkte+" Fault points")
+  $(".section__header h1").html("["+questionId+"] "+drivingSchool.questions[questionId].question+"")
+  $(".section__header .faultpoints").html(""+drivingSchool.questions[questionId].faultpoints+" Fault points")
 
-  fahrschule.fragen[questionId].antworten.forEach(function(antwort, i) {
+  drivingSchool.questions[questionId].answer.forEach(function(antwort, i) {
     $(".section__checkboxes").append('<label class="checkbox">'+antwort[0]+'<input type="checkbox" name="'+i+'"><span class="checkmark"></span></label>')
   });
 }
 
 function resetQuestion() {
   $(".section__header h1").html("")
-  $(".section__header .fehlerpunkte").html("")
+  $(".section__header .faultpoints").html("")
   $(".section__checkboxes").html("")
 }
 
 function updateStepProgress() {
-  let progress = (100 / Object.keys(fahrschule.fragen).length);
-  let currentProgress = (fahrschule.aktuelleFrage-1) * progress;
+  let progress = (100 / Object.keys(drivingSchool.questions).length);
+  let currentProgress = (drivingSchool.currentQuestion-1) * progress;
   $(".step-progress__progress").animate({ width: ""+currentProgress+"%" }, 1000 );
 }
