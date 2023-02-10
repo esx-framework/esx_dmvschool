94cf2c8873f60bc139ebd731220c292f6c6f8010
Zan1456 <62830223+Zan1456@users.noreply.github.com>
2023-02-10 19:15:25 +0100

    .

---
+++ html/locales/locale_hu.js
@@ -0,0 +1,16 @@
+var colorCodes = {
+    fault_point : "Hiba pontok",
+    error_msg : "Hiba - Adj meg egy választ a folytatáshoz",
+    // Unsuccessful test completion
+    unsuccess_title : "Hoppá, ez valószínűleg semmi volt!",
+    unsuccess_desciption : "Sajnos nem tetted le az elméleti vezetői vizsgát.",
+    unsuccess_text : "Szívesen megismételheted az elméleti vezetői vizsgát..",
+    unsuccess_text2 : "Reméljük legközelebbre jobban felkészülsz.",
+    unsuccess_text3 : "Köszönjük a részvételt!",
+    // Successful test completion
+    success_title : "Gratulálunk!",
+    success_desciption : "Sikeresen letetted az elméleti vezetői vizsgát.",
+    success_text : "Most már leteheted a gyakorlati vizsgát is.",
+    success_text2 : "Jó vezetést kívánunk.",
+    success_text3 : "Köszönjük a részvételt!",
+}
