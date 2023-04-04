42adc5d7485597588afefbed435a6d60e8e74103
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-04-04 23:03:27 +0200

    Update nl_esx_dmvschool.sql

--- localization/nl_esx_dmvschool.sql
+++ localization/nl_esx_dmvschool.sql
@@ -1,6 +1,6 @@
 INSERT INTO `licenses` (`type`, `label`) VALUES
 	('dmv', 'Theorie examen'),
 	('drive', 'Rijbewijs B'),
-	('drive_bike', 'Motor Rijbewijs'),
-	('drive_truck', 'Vrachtwagen Rijbewijs')
+        ('drive_bike', 'Rijbewijs A'),
+	('drive_truck', 'Rijbewijs C')
 ;
